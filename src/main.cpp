9d3077221d49973b2a5e209c5af84ad8d190941e
Philippe Giacinti <philippe@giacinti.com>
2019-08-27 22:35:15 +0200

    simple video stream

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,27 @@
 #include <WiFiClient.h>
 #include <AutoConnect.h>
 
+#define BOUNDARY String("--boundarydonotcross")
+
 OV2640 cam;
 WebServer server(80);
 AutoConnect portal(server);
 
+void message(String msg)
+{
+    Serial.println(msg);
+}
+
 void handle_stream(void)
 {
+	#if 0
+	String args=server.arg(0);
+	message("arg, name="+server.argName(0)+" value="+server.arg(0));
+	#endif
+	
     WiFiClient client = server.client();
     String response = "HTTP/1.1 200 OK\r\n";
-    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+    response += "Content-Type: multipart/x-mixed-replace; boundary="+BOUNDARY+"\r\n\r\n";
     server.sendContent(response);
 
     while (1)
@@ -20,7 +32,7 @@ void handle_stream(void)
         cam.run();
         if (!client.connected())
             break;
-        response = "--frame\r\n";
+        response = BOUNDARY+"\r\n";
         response += "Content-Type: image/jpeg\r\n";
         response += "Content-Length: " + String(cam.getSize()) + "\r\n\r\n";
         server.sendContent(response);
@@ -31,6 +43,23 @@ void handle_stream(void)
             break;
     }
 }
+
+#if 0
+void handle_image(void)
+{
+	WiFiClient client = server.client();
+
+    cam.run();
+    if (!client.connected())
+        return;
+    String response = "HTTP/1.1 200 OK\r\n";
+    response += "Content-disposition: inline; filename=capture.jpg\r\n";
+    response += "Content-type: image/jpeg\r\n";
+    response += "Content-Length: " + String(cam.getSize()) + "\r\n\r\n";
+    server.sendContent(response);
+    client.write((char *)cam.getfb(), cam.getSize());
+}
+#endif
 
 void handleNotFound()
 {
@@ -45,10 +74,6 @@ void handleNotFound()
     server.send(200, "text/plain", message);
 }
 
-void message(String msg)
-{
-    Serial.println(msg);
-}
 
 void setup() {
 	Serial.begin(115200);
@@ -60,9 +85,10 @@ void setup() {
     esp32cam_aithinker_config.xclk_freq_hz = 10000000;
     esp32cam_aithinker_config.frame_size = FRAMESIZE_VGA;
     esp32cam_aithinker_config.jpeg_quality = 10;
-    int camInit = cam.init(esp32cam_aithinker_config);
+    /*int camInit = */cam.init(esp32cam_aithinker_config);
    
     server.on("/", HTTP_GET, handle_stream);
+	// server.on("/image", HTTP_GET, handle_image);
     server.onNotFound(handleNotFound);
 	
     portal.begin();
